10e121bd08b8d7f8edd8b14ae67f370dda069939
Aditya Dwivedi <dwikaljit@gmail.com>
2024-04-20 16:46:34 +0530

    displayed product list by creating a grid layout using react bootstrap's Row and Column

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,46 @@
-import './App.css'
+import {Product} from "./types/Product.ts";
+import {sampleProducts} from "./data.ts";
+import {Component} from "react";
+import {Col, Container, Nav, Navbar, Row} from "react-bootstrap";
 
-function App() {
+class App extends Component {
+  render() {
     return (
-        <div>
-            <header>
-                System Shop
-            </header>
-            <main>
-                Hello, World!
-            </main>
-            <footer>
-                All rights reserved.
-            </footer>
-        </div>
+      <div className='d-flex flex-column vh-100'>
+        <header>
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+              <Navbar.Brand>SystemShop</Navbar.Brand>
+            </Container>
+            <Nav>
+              <a href="/cart" className="nav-link">Cart</a>
+              <a href="/signin" className="nav-link">Sign In</a>
+            </Nav>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Row>
+              {sampleProducts.map((product: Product) => (
+                <Col key={product.slug} sm={6} md={4} lg={3}>
+                  <img
+                    src={product.image}
+                    alt={product.name}
+                    className="product-image"
+                  />
+                  <h2>{product.name}</h2>
+                  <p>${product.price}</p>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
     )
+  }
 }
 
 export default App
