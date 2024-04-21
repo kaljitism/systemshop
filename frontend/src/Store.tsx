07adeec3c59d17a796b5dfeea70e9c8c2e92cba6
Aditya Dwivedi <dwikaljit@gmail.com>
2024-04-21 20:28:49 +0530

    Worked with State, Actions and Reducer to facilitate addToCart

--- frontend/src/Store.tsx
+++ frontend/src/Store.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import {Cart, CartItem} from "./types/Cart.ts";
 
 type AppState = {
   mode: string
+  cart: Cart
 }
 
 const initialState: AppState = {
@@ -10,16 +12,46 @@ const initialState: AppState = {
     : window.matchMedia && window.matchMedia('prefers-color-scheme: dark').matches
       ? 'dark'
       : 'light',
+  cart: {
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems')!)
+      : [],
+    shippingAddress: localStorage.getItem('shippingAddress')
+      ? JSON.parse(localStorage.getItem('shippingAddress')!)
+      : {},
+    paymentMethod: localStorage.getItem('paymentMethod')
+      ? localStorage.getItem('paymentMethod')!
+      : 'Paypal',
+    // TODO: Work these values out
+    itemsPrice: 0,
+    shippingPrice: 0,
+    taxPrice: 0,
+    totalPrice: 0
+  }
+
 }
 
-type Action = { type: 'SWITCH_MODE' }
+type Action =
+  | { type: 'SWITCH_MODE' }
+  | { type: 'CART_ADD_ITEM', payload: CartItem }
 
 function reducer(state: AppState, action: Action): AppState {
-  switch (action.type) {
-    case "SWITCH_MODE":
-      return {mode: state.mode === 'dark' ? 'light' : 'dark'}
-    default:
-      return state
+  if (action.type === 'SWITCH_MODE') {
+    return {...state, mode: state.mode === 'dark' ? 'light' : 'dark'}
+  } else if (action.type === 'CART_ADD_ITEM') {
+    const newItem = action.payload
+    const existItem = state.cart.cartItems.find(
+      (item: CartItem) => item._id == newItem._id
+    )
+    const cartItems = existItem
+      ? state.cart.cartItems.map((item: CartItem) =>
+        item._id === existItem._id ? newItem : item
+      )
+      : [...state.cart.cartItems, newItem]
+    localStorage.setItem('cartItems', JSON.stringify(cartItems))
+    return {...state, cart: {...state.cart, cartItems}}
+  } else {
+    return state
   }
 }
 
